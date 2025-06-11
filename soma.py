ae47bb3bb04f0f9be8370bb687710aefc62587d7
caioportela-jpg <caioportela77@gmail.com>
2025-06-10 22:54:31 -0300

    Update soma.py

--- soma.py
+++ soma.py
@@ -1,2 +1,6 @@
 def soma(e,b):
     return e+b
+    
+numero1 = int(input('Escolha o seu primeiro número: ')), numero2 = int(input('Escolha seu segundo número: '))
+soma = soma(numero1, numero2)
+print('O resultado da sua soma do número ', numero1, 'com', numero2, 'é igual a ', soma)
